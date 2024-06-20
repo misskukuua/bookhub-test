4206e4988262207c6eab22410f7c0bf297c09c02
misskukuua <p.asare@alustudent.com>
2024-06-20 16:36:30 +0000

    deleted unwanted files

--- bookhub_frontend/book-hub/src/App.tsx
+++ bookhub_frontend/book-hub/src/App.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import Navbar from './components/Navbar';
-import BooksPage from './pages/BooksPage';
 import CreatePage from './pages/CreatePage'
 
 
@@ -12,7 +11,6 @@ const App: React.FC = () => {
       <Navbar />
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/books" element={<BooksPage />} />
         <Route path="/create" element={<CreatePage/>} />
         {/* Add other routes here */}
       </Routes>
